0df92de6b56434ea5d4aba98baadaefc73c3007e
Daniel Gasienica <daniel@gasienica.ch>
2019-05-09 00:48:46 -0400

    Annotate `counter` example

--- counter/src/Counter/Counter.jsx
+++ counter/src/Counter/Counter.jsx
@@ -2,6 +2,7 @@ import React from "react"
 
 export const Counter = props =>
   <div>
+    <h1>Counter (JSX)</h1>
     Count: {props.count}<br/>
     <button type="button" onClick={props.onInc}>Inc</button>
     <button type="button" onClick={props.onDec}>Dec</button>
