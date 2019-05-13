af1e72f138d310cffc1a25ac31910477742c1d9f
Fyodor Soikin <name.fa@gmail.com>
2019-05-13 11:26:20 -0400

    Add frame, two-counter example, Bootstrap (#3)

--- counter/src/Counter/Counter.jsx
+++ counter/src/Counter/Counter.jsx
@@ -1,8 +1,14 @@
 import React from "react"
+import { Row, Col, Button, Alert, Card } from "react-bootstrap"
 
 export const Counter = props =>
-  <div>
-    Count: {props.count}<br/>
-    <button type="button" onClick={props.onInc}>Inc</button>
-    <button type="button" onClick={props.onDec}>Dec</button>
-  </div>
+  <Row>
+    <Col md={2}>
+      <Row><Col>Count</Col></Row>
+      <Row><Col><h2>{props.count}</h2></Col></Row>
+    </Col>
+    <Col md={3}>
+      <Button onClick={props.onInc} block>Inc</Button>
+      <Button onClick={props.onDec} block>Dec</Button>
+    </Col>
+  </Row>
--- counter/src/Main.purs
+++ counter/src/Main.purs
@@ -2,13 +2,18 @@ module Main where
 
 import Prelude
 
-import Counter.Counter as Counter
+import Counter as Counter
 import Data.Function.Uncurried (runFn2)
 import Data.Maybe (Maybe(..))
+import Data.Traversable (for)
 import Effect (Effect)
+import Effect.Aff (Aff)
 import Effect.Console (log)
+import Elmish (ComponentDef)
 import Elmish as Elmish
 import Elmish.React as React
+import Frame as Frame
+import TwoCounters as TwoCounters
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
@@ -18,8 +23,28 @@ main :: Effect Unit
 main = do
   el <- getElementById "app" =<< (map toNonElementParentNode <<< document) =<< window
   case el of
-    Nothing -> log "Can't find #app element"
-    Just e -> runFn2 React.reactMount e =<< Elmish.construct Counter.def onError
+    Nothing ->
+        log "Can't find #app element"
+    Just e -> do
+        items <- for examples mkItem
+        runFn2 React.reactMount e =<< Elmish.construct (Frame.frame items) onError
 
   where
     onError = log
+
+    mkItem :: Example -> Effect Frame.Item
+    mkItem { title, create } = do
+        view <- create \c -> Elmish.construct c onError
+        pure { title, view }
+
+
+type Example =
+    { title :: String
+    , create :: forall a. (forall m s. ComponentDef Aff m s -> Effect a) -> Effect a
+    }
+
+examples :: Array Example
+examples =
+    [ { title: "Simple counter", create: \f -> f Counter.def }
+    , { title: "Two counters", create: \f -> f TwoCounters.def }
+    ]
