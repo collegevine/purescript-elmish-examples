af31f97d9d96d112da6d842bb1595932b81f53eb
Fyodor Soikin <name.fa@gmail.com>
2019-05-28 18:05:18 -0400

    Upgrade to Elmish v0.0.5 (#8)

--- counter/src/Main.purs
+++ counter/src/Main.purs
@@ -3,44 +3,29 @@ module Main where
 import Prelude
 
 import Counter as Counter
-import Data.Function.Uncurried (runFn2)
-import Data.Maybe (Maybe(..))
 import Data.Traversable (for)
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Console (log)
-import Elmish (ComponentDef)
 import Elmish as Elmish
-import Elmish.React as React
+import Elmish.Component (ComponentReturnCallback)
+import Elmish.Dispatch (dispatchMsgFn)
 import Frame as Frame
 import TwoCounters as TwoCounters
-import Web.DOM.NonElementParentNode (getElementById)
-import Web.HTML (window)
-import Web.HTML.HTMLDocument (toNonElementParentNode)
-import Web.HTML.Window (document)
 
 main :: Effect Unit
 main = do
-  el <- getElementById "app" =<< (map toNonElementParentNode <<< document) =<< window
-  case el of
-    Nothing ->
-        log "Can't find #app element"
-    Just e -> do
-        items <- for examples mkItem
-        runFn2 React.reactMount e =<< Elmish.construct (Frame.frame items) onError
-
-  where
-    onError = log
-
-    mkItem :: Example -> Effect Frame.Item
-    mkItem { title, create } = do
-        view <- create \c -> Elmish.construct c onError
-        pure { title, view }
+    items <- for examples \e -> do
+        render <- e.create Elmish.construct
+        pure { title: e.title, view: render onError }
+    Elmish.boot { domElementId: "app", def: Frame.frame items }
+    where
+        onError = dispatchMsgFn log (const $ pure unit)
 
 
 type Example =
     { title :: String
-    , create :: forall a. (forall m s. ComponentDef Aff m s -> Effect a) -> Effect a
+    , create :: forall a. ComponentReturnCallback Aff (Effect a) -> Effect a
     }
 
 examples :: Array Example
--- html-hello-world/src/Main.purs
+++ html-hello-world/src/Main.purs
@@ -2,23 +2,16 @@ module Main where
 
 import Prelude
 
+import Effect (Effect)
+import Elmish (boot)
 import HTMLHelloWorld (helloWorld)
 
-import Data.Function.Uncurried (runFn2)
-import Data.Maybe (Maybe(..))
-import Effect (Effect)
-import Effect.Console as Console
-import Elmish.React as React
-import Web.DOM.NonElementParentNode (getElementById)
-import Web.HTML (window)
-import Web.HTML.HTMLDocument (toNonElementParentNode)
-import Web.HTML.Window (document)
-
 main :: Effect Unit
-main = do
-  mApp <- getElementById "app" =<< (map toNonElementParentNode <<< document) =<< window
-  case mApp of
-    Nothing ->
-        Console.error "Can’t find #app element"
-    Just app ->
-        runFn2 React.reactMount app helloWorld
+main = boot
+    { domElementId: "app"
+    , def:
+        { init: pure unit
+        , update: \_ _ -> pure unit
+        , view: \_ _ -> helloWorld
+        }
+    }
